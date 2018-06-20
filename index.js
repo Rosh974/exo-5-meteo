2d229d40a7573d7a2ecb1bf3ec93672e7e59e768
Rosh974 <didier.vilrus1@gmail.com>
2018-06-20 09:11:08 +0400

    finalisation avec résolution du port et de l'affichage dans le serveur

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-
+const express = require ('express');
 const request = require('request');
-const argv = require('yargs').argv;
+const app = express();
+
 
 // Nous définissons ici les paramètres du serveur.
-var hostname = 'localhost'; 
-var port = 3064; 
- 
-var app = request(); 
- 
-// Démarrer le serveur 
-app.listen(port, hostname, function(){
-	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port+"\n"); 
-});
- 
+var port = 3064;
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html")
+})
 
 let apiKey = '368214adc9ba08a2491b6087d7950f49';
-let city = argv.c || 'Ile de la Réunion';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+// let city = argv.c || 'Ile de la Réunion';
+let url = `http://api.openweathermap.org/data/2.5/group?id=524901,6690283,2988507,2995469&units=metric&appid=${apiKey}`
 
-request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
-  } else {
-    let weather = JSON.parse(body)
-    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-    console.log(message);
-  }
+app.get('/test', function (req, res) {
+
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log('error:', error);
+    } else {
+      let weather = JSON.parse(body)
+      // let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+      console.log(weather);
+      res.json(weather)
+    }
+  });
+
+
+
+})
+
+
+// Démarrer le serveur 
+app.listen(port, function () {
+  console.log("Mon serveur fonctionne");
 });
-
--- public/script.js
+++ public/script.js
@@ -2,11 +2,16 @@
 
         $.ajax({
             type: 'GET',
-            url: "http://localhost:3064",
+            url: "http://localhost:3064/test",
             success: function (data) {
-                
-            
-                console.log("ok");
+                console.log(data);
+                var liste = data.list
+                liste.forEach(function(liste){
+                    console.log(liste);
+                    $('#m').append( '<p>' + "" + liste.name + '<br/>' + 'température : ' +  liste.main.temp + '</p>')
+                })
+               
+               
             },
  
 });  
